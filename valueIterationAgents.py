2dca4783bfb988b3194c58b677669fd4c94f6261
vladiki <vlad2mail@gmail.com>
2013-03-04 01:56:40 -0800

    started valueIterationAgent

--- valueIterationAgents.py
+++ valueIterationAgents.py
@@ -41,6 +41,9 @@ class ValueIterationAgent(ValueEstimationAgent):
 
         # Write value iteration code here
         "*** YOUR CODE HERE ***"
+        for i in range(self.iterations):
+          self.computeActionFromValues(self.mdp.getStartState())
+
 
 
     def getValue(self, state):
@@ -56,7 +59,10 @@ class ValueIterationAgent(ValueEstimationAgent):
           value function stored in self.values.
         """
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        q_value = 0
+        for nextState, prob in self.mdp.getTransitionStatesAndProbs(state,action):
+          q_value += prob*(self.mdp.getReward(state,action,nextState)+self.discount*self.getValue(nextState))
+        return q_value
 
     def computeActionFromValues(self, state):
         """
@@ -68,7 +74,9 @@ class ValueIterationAgent(ValueEstimationAgent):
           terminal state, you should return None.
         """
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        for action in self.mdp.getPossibleActions(state):
+          self.values[state] += self.computeQValueFromValues(state,action)
+        return None
 
     def getPolicy(self, state):
         return self.computeActionFromValues(state)
